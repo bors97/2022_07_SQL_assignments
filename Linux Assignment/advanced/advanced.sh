a439e7cbda41eda258686a6c0a69f492ea7b4072
Bors Szlivka <bors97@gmail.com>
2022-07-19 21:06:44 +0200

    Update

--- Linux Assignment/advanced/advanced.sh
+++ Linux Assignment/advanced/advanced.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 . ./config.cfg
 
-echo "$dbhost $dbname"
+IFS=";"
 
-PGPASSWORD=$password psql -h $dbhost -d $dbname -U $username << EOF
+for query in $queries; do
+	echo "$query"
+	IFS=':'
+	read -r -a array <<< "$query"
+	echo "${array[2]}"
+	IFS=" "
+	read -r -a ${array[2]} <<< ${array[2]}
+	
+	PGPASSWORD=$password psql -h $dbhost -d $dbname -U $username << EOF
+		
+	EOF
+done
 
-EOF
