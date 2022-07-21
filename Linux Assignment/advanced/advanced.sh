cc868d55f6af49d25bd930a05223e5ae56657954
Bors Szlivka <bors97@gmail.com>
2022-07-21 11:06:25 +0200

    Power Bi and some advanced linux stuff I think

--- Linux Assignment/advanced/advanced.sh
+++ Linux Assignment/advanced/advanced.sh
@@ -3,16 +3,20 @@
 
 IFS=";"
 
-for query in $queries; do
-	echo "$query"
-	IFS=':'
-	read -r -a array <<< "$query"
-	echo "${array[2]}"
-	IFS=" "
-	read -r -a ${array[2]} <<< ${array[2]}
-	
-	PGPASSWORD=$password psql -h $dbhost -d $dbname -U $username << EOF
-		
-	EOF
-done
+
+echo $query
+
+
+#for query in $queries; do
+#	echo "$query"
+#	IFS=':'
+#	read -r -a array <<< "$query"
+#	echo "${array[2]}"
+#	IFS=" "
+#	read -r -a ${array[2]} <<< ${array[2]}
+#	
+#	PGPASSWORD=$password psql -h $dbhost -d $dbname -U $username << EOF
+#		
+#	EOF
+#done
 
--- Linux Assignment/dataLogger.sh
+++ Linux Assignment/dataLogger.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 echo "Store new data in here" > demand_data.txt
 date >> demands.log
